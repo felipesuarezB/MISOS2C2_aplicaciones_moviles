a09df65eef2a0798722e60d69d2588b3d82a0e6b
angello.villegascuartas <angello.villegascuartas@wbdcontractor.com>
2025-04-15 22:24:50 -0500

    chore: adds CollectorDetailView

--- app/src/main/java/com/example/viniloapp/ui/collectors/CollectorDetailFragment.kt
+++ app/src/main/java/com/example/viniloapp/ui/collectors/CollectorDetailFragment.kt
@@ -1,37 +1,44 @@
 package com.example.viniloapp.ui.collectors
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.example.viniloapp.databinding.FragmentCollectorDetailBinding
-import com.example.viniloapp.ui.adapters.CollectorDetailAdapter
+import com.example.viniloapp.viewmodels.CollectorDetailViewModel
 
 
 class CollectorDetailFragment: Fragment() {
     private var _binding: FragmentCollectorDetailBinding? = null
     private val binding get() = _binding!!
-<<<<<<< Updated upstream
-=======
-    private lateinit var viewModelAdapter: CollectorDetailAdapter
->>>>>>> Stashed changes
-    private var collectorId: Int = 0
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        collectorId = arguments?.getInt("collectorId") ?: collectorId
-    }
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewModel: CollectorDetailViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        Log.d("CollectorDetailFragment", "Creating Collector Detail view")
         _binding = FragmentCollectorDetailBinding.inflate(inflater, container, false)
-        viewModelAdapter = CollectorDetailAdapter()
         return  binding.root
 
+    }
+
+    override fun onViewCreated(view:View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(CollectorDetailViewModel::class.java)
+        viewModel.collectorDetail.observe(viewLifecycleOwner, Observer {
+            collectorDetail ->
+            collectorDetail?.let {
+                binding.collectorDetail = collectorDetail
+            }
+        })
     }
 
     override fun onDestroyView() {
---
+++ app/src/main/java/com/example/viniloapp/viewmodels/CollectorDetailViewModel.kt
@@ -0,0 +1,37 @@
+package com.example.viniloapp.viewmodels
+
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.viniloapp.models.CollectorDetail
+
+class CollectorDetailViewModel(application: Application): AndroidViewModel(application) {
+    private val _collectorDetail = MutableLiveData<CollectorDetail>()
+    val collectorDetail: LiveData<CollectorDetail> = _collectorDetail
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    private val _error = MutableLiveData<String>()
+    val error: LiveData<String> = _error
+
+    init {
+        Log.d("CollectorDetailViewModel", "Initializing view model")
+        _collectorDetail.value = CollectorDetail(1, "any", "30002", "test@test.com")
+    }
+
+    class Factory(private val application: Application): ViewModelProvider.Factory {
+        @Suppress("UNCHECKED_CAST")
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(CollectorViewModel::class.java)) {
+                return CollectorDetailViewModel(application) as T
+            }
+            throw IllegalArgumentException("Unknown viewModel Class")
+        }
+    }
+
+}

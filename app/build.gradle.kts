dcbdb554849009b6dddca03a62a6b5c90215fa40
Felipe Suárez <felipesuarezbe@hotmail.com>
2025-04-22 21:42:31 -0500

    Creacion pruebas E2E (#14)
    * Desarrollo HU05
    
    * Pruebas E2E vinilos

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,6 @@ android {
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -33,6 +32,7 @@ android {
     }
     kotlinOptions {
         jvmTarget = "11"
+        task("testClasses")
     }
     buildFeatures {
         dataBinding = true
@@ -50,9 +50,14 @@ dependencies {
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
     implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation(libs.androidx.espresso.contrib)
     kapt("com.github.bumptech.glide:compiler:4.16.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("com.android.volley:volley:1.2.1")
+    androidTestImplementation("androidx.test:runner:1.5.2")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test:rules:1.5.0")
 }

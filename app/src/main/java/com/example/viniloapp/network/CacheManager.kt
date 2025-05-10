8a7a9987a1cb0221714ba48fc1e8feeb30cb1ed2
angello.villegascuartas <angello.villegascuartas@wbdcontractor.com>
2025-05-10 15:06:36 -0500

    chore: adds repository pattern and caching strategy to albums

--- app/src/main/java/com/example/viniloapp/network/CacheManager.kt
+++ app/src/main/java/com/example/viniloapp/network/CacheManager.kt
@@ -2,13 +2,14 @@ package com.example.viniloapp.network
 
 import android.content.Context
 import android.util.LruCache
+import com.example.viniloapp.models.Album
+import com.example.viniloapp.models.AlbumDetail
 import com.example.viniloapp.models.Collector
 import com.example.viniloapp.models.CollectorDetail
 
 class CacheManager(context: Context) {
     companion object {
         var instance: CacheManager? = null
-        private var cacheCollectorsMiliseconds: Long = 0L
         private val cacheDurationMillis = 60_000L
         fun getInstance(context: Context) = instance ?: synchronized(this) {
             instance ?: CacheManager(context).also {
@@ -17,6 +18,7 @@ class CacheManager(context: Context) {
         }
     }
 
+    // Collector cache
     private var collectorDetails: LruCache<Int, CollectorDetail> = LruCache(3)
     fun cacheCollectorDetails(collectorId: Int, collector: CollectorDetail){
         if (collectorDetails[collectorId] == null){
@@ -24,18 +26,19 @@ class CacheManager(context: Context) {
         }
     }
     fun getCollectorDetails(collectorId: Int) : CollectorDetail? {
-        return if (collectorDetails[collectorId]!=null) collectorDetails[collectorId]!! else null
+        return collectorDetails[collectorId]
     }
 
+    private var cacheCollectorsMilliseconds: Long = 0L
     private var collectors: List<Collector>? = null
     fun cacheCollectors(collectors: List<Collector>){
-        cacheCollectorsMiliseconds = System.currentTimeMillis()
+        cacheCollectorsMilliseconds = System.currentTimeMillis()
         this.collectors = collectors
     }
 
     fun getCollectors() : List<Collector>? {
         val now = System.currentTimeMillis()
-        return if (now - cacheCollectorsMiliseconds < cacheDurationMillis) {
+        return if (now - cacheCollectorsMilliseconds < cacheDurationMillis) {
             collectors
         } else {
             collectors = null
@@ -48,5 +51,37 @@ class CacheManager(context: Context) {
         collectors = null
     }
 
+    // Album cache
+    private var albumDetail: LruCache<Int, AlbumDetail> = LruCache(3)
+    fun cacheAlbumDetail(albumId: Int, album: AlbumDetail){
+        if (albumDetail[albumId] == null){
+            albumDetail.put(albumId, album)
+        }
+    }
+
+    fun getAlbumDetail(albumId: Int):AlbumDetail? {
+        return albumDetail[albumId]
+    }
+
+    private var cacheAlbumsMilliseconds: Long = 0L
+    private var albums: List<Album>? = null
+    fun cacheAlbums(albums: List<Album>){
+        cacheAlbumsMilliseconds = System.currentTimeMillis()
+        this.albums = albums
+    }
+
+    fun getAlbums() : List<Album>? {
+        val now = System.currentTimeMillis()
+        return if (now - cacheAlbumsMilliseconds < cacheDurationMillis) {
+            albums
+        } else {
+            albums = null
+            null
+        }
+    }
+
+    fun clearAlbumCache() {
+        albums = null;
+    }
 
 }

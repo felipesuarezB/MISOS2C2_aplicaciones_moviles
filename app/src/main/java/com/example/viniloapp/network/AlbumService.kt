f9b303850a20f284ec7144982dc2808e952325a3
Santiago Gómez Perdomo <s.gomezp2345@uniandes.edu.co>
2025-05-10 21:50:47 -0500

    refactor 4

--- app/src/main/java/com/example/viniloapp/network/AlbumService.kt
+++ app/src/main/java/com/example/viniloapp/network/AlbumService.kt
@@ -5,68 +5,26 @@ import com.example.viniloapp.MyApplication
 import com.example.viniloapp.models.Album
 import com.example.viniloapp.models.AlbumDetail
 import com.example.viniloapp.models.Track
-import org.json.JSONArray
-import org.json.JSONObject
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 class AlbumService(
     private val networkServiceAdapter: NetworkServiceAdapter =
         NetworkServiceAdapter.getInstance(MyApplication.getAppContext())
 ) {
 
-
     suspend fun getAlbums(): List<Album> {
-        Log.d("AlbumService", "Parsing")
-        val resp = JSONArray(networkServiceAdapter.get("albums"))
-        val list = mutableListOf<Album>()
-
-        for (i in 0 until resp.length()) {
-            list.add(
-                Album(
-                    id = resp.getJSONObject(i).getInt("id"),
-                    name = resp.getJSONObject(i).getString("name"),
-                    cover = resp.getJSONObject(i).getString("cover"),
-                    recordLabel = resp.getJSONObject(i).getString("recordLabel"),
-                    releaseDate = resp.getJSONObject(i).getString("releaseDate"),
-                    genre = resp.getJSONObject(i).getString("genre"),
-                    description = resp.getJSONObject(i).getString("description")
-                )
-            )
-        }
-        return list
+        val json = networkServiceAdapter.get("albums")
+        val type = object : TypeToken<List<Album>>() {}.type
+        return Gson().fromJson(json, type)
     }
 
-    suspend fun getAlbumDetail(albumId: Int):AlbumDetail {
-        val albumJson = JSONObject(networkServiceAdapter.get("albums/$albumId"))
-        val tracksArray = albumJson.getJSONArray("tracks")
-        val tracks = mutableListOf<Track>()
-
-        for (i in 0 until tracksArray.length()) {
-            Log.d("NetworkServiceAdapter", "Processing track: ${tracksArray.getJSONObject(i).getString("name")}")
-            tracks.add(
-                Track(
-                    id = tracksArray.getJSONObject(i).getInt("id"),
-                    name = tracksArray.getJSONObject(i).getString("name"),
-                    duration = tracksArray.getJSONObject(i).getString("duration")
-                )
-            )
-        }
-
-        Log.d("NetworkServiceAdapter", "Total tracks processed: ${tracks.size}")
-
-        return AlbumDetail(
-            id = albumJson.getInt("id"),
-            name = albumJson.getString("name"),
-            cover = albumJson.getString("cover"),
-            releaseDate = albumJson.getString("releaseDate"),
-            description = albumJson.getString("description"),
-            genre = albumJson.getString("genre"),
-            recordLabel = albumJson.getString("recordLabel"),
-            tracks = tracks
-        )
+    suspend fun getAlbumDetail(albumId: Int): AlbumDetail {
+        val json = networkServiceAdapter.get("albums/$albumId")
+        return Gson().fromJson(json, AlbumDetail::class.java)
     }
 
     suspend fun createAlbum(jsonBody: JSONObject) {
         networkServiceAdapter.post("albums", jsonBody)
     }
-
-} 
+}

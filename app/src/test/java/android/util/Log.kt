167c0e80583f1bd854d545173f7a13baea7acf64
Santiago Gómez Perdomo <s.gomezp2345@uniandes.edu.co>
2025-05-10 21:44:28 -0500

    refactor 3

--- app/src/test/java/android/util/Log.kt
+++ app/src/test/java/android/util/Log.kt
@@ -1,9 +1,11 @@
 package android.util
 
-object Log {
-    fun d(tag: String?, msg: String?): Int = 0
-    fun e(tag: String?, msg: String?): Int = 0
-    fun i(tag: String?, msg: String?): Int = 0
-    fun w(tag: String?, msg: String?): Int = 0
-    fun v(tag: String?, msg: String?): Int = 0
+class Log {
+    companion object {
+        @JvmStatic fun d(tag: String?, msg: String?): Int = 0
+        @JvmStatic fun e(tag: String?, msg: String?): Int = 0
+        @JvmStatic fun i(tag: String?, msg: String?): Int = 0
+        @JvmStatic fun w(tag: String?, msg: String?): Int = 0
+        @JvmStatic fun v(tag: String?, msg: String?): Int = 0
+    }
 }
